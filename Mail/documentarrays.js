883ce76f7ffaf62f949cf4ac99e1ee06e3fa005b
Johanna  Halfmann <Johanna.Halfmann@haw-hamburg.de>
2016-06-10 20:42:12 +0200

    2/7 Mails ausformuliert + Zeitungsnamen

--- Mail/documentarrays.js
+++ Mail/documentarrays.js
@@ -2,8 +2,9 @@
 var zDokumentText=new Array(22);
 var zQuelle=new Array(22);
 
-zDokumentText[3]="Skandal 4: Ein Stellvertreter von Herr Meyer bestätigt Sicherheitslücken bei einem Interview.";
-zDokumentText[4]="Skandal 5: Herr Meyer gibt Sicherheitslücken bei einem persönlichen Interview zu.";
+/* was sind zDokumentText[3] und [4] - wann wird das gebraucht? */
+zDokumentText[3]="Skandal 4: Ein Stellvertreter von Herr Mayer bestätigt Sicherheitslücken bei einem Interview.";
+zDokumentText[4]="Skandal 5: Herr Mayer gibt Sicherheitslücken bei einem persönlichen Interview zu.";
 
 zDokumentText[7]="(Skandal8)  Herr Meyer gibt nach Interview die Beteiligung der Firma zu.";
 zDokumentText[9]="(Skandal10) Herr Meyer streitet nach Interview die Beteiligung der Firma ab.";
@@ -12,19 +13,30 @@ zDokumentText[13]="(Skandal14) Herr Meyer streitet nach Interview die Beteiligun
 zDokumentText[14]="(Skandal15) Sehr negativer Bericht.";
 
 
-zDokumentText[19]="Artikel Intern";
-zDokumentText[20]="Artikel Extern";
+zDokumentText[19]="„Die Mayermetall GmbH freut sich außerordentlich verkünden zu dürfen, dass "+ 
+					"der langjährige Kollege Herr Heinz Bartelsmann zum 1. September die Stelle des Personalchefs übernehmen wird.“ "+
+					"Mit diesen Worten verrät Pressesprecher Knoll, was Viele erwartet haben. Durch seine "+
+					"konsequente und sichere Leitung führte er seine Abteilung stets zu Wachstum und überzeugte mit langem Atem. "+
+					"Dem pensionierten Herrn Mutmacher wünscht die IndustrieMarkt-Redaktion einen lebhaften und ausgeglichenen Ruhestand! ";
+zDokumentText[20]="„Die Mayermetall GmbH freut sich außerordentlich verkünden zu dürfen, 
+					"dass Herr Markus Schmitz zum 1. September die Stelle des Personalchefs übernehmen wird.“ "+
+					"Mit diesen Worten verrät Pressesprecher Knoll, was nur Wenige erwartet haben. "+
+					"Schmitz führte in den vergangen Jahren erfolgreich die Personalabteilung bei der "+ 
+					"Süderholz in Berlin-Spandau. Innovatives Personalmanagement trifft hier auf "+
+					"hanseatische Tradition - eine gewagte, dich sicher geplante Kombination. "+
+					"Dem pensionierten Herrn Mutmacher wünscht die IndustrieMarkt-Redaktion "+
+					"einen lebhaften und ausgeglichenen Ruhestand! ";
 
 zQuelle[3]="Zeitung 6";
 zQuelle[4]="Zeitung 7";
-zQuelle[7]="Zeitung 3";
-zQuelle[9]="Zeitung 4";
-zQuelle[12]="Zeitung 8";
-zQuelle[13]="Zeitung 9";
-zQuelle[14]="Zeitung 5";
+zQuelle[7]="Stader Morgenblatt";
+zQuelle[9]="Stader Morgenblatt";
+zQuelle[12]="Hamburger Abendpost";
+zQuelle[13]="Hamburger Abendpost";
+zQuelle[14]="Norddeutsche Zeitung";
 
-zQuelle[19]="Zeitung 1";
-zQuelle[20]="Zeitung 2";
+zQuelle[19]="IndustrieMarkt";
+zQuelle[20]="IndustrieMarkt";
 
 /*
 Norddeutsche Zeitung
