bcf77169b4d9ef845cc275b1d4634ebe02805438
Johanna  Halfmann <Johanna.Halfmann@haw-hamburg.de>
2016-07-06 14:56:11 +0200

    alle Artikel ausformuliert

--- Mail/documentarrays.js
+++ Mail/documentarrays.js
@@ -6,24 +6,25 @@ var zTitel=new Array(22);
 /* was sind zDokumentText[3] und [4] - wann wird das gebraucht? */
 zDokumentText[0]="";
 
-zDokumentText[3]="Sicherheitslücken bei Mayermetall! Im Interview gab Wolfgang "+
- 					"Maria Mayer nun zu, dass es signifikante Sicherheitsmängel "+
- 					"in der verunglückten Mine gabt. Wie es dazu kommen konnte und "+
- 					"warum die Hamburger Verantwortlichen nicht darüber in Kenntnis gesetzt "+
- 					"wurden, dazu konnte er keine Auskunft geben. Beim Unglück im vergangenen " +
- 					"Monat kamen 41 Menschen ums Leben, mehr als 200 wurden verletzt.";
-zDokumentText[4]="Sicherheitslücken bei Mayermetall! Im Interview gab ein Stellvertreter von Wolfgang "+
- 					"Maria Mayer nun zu, dass es signifikante Sicherheitsmängel "+
- 					"in der verunglückten Mine gabt. Wie es dazu kommen konnte und "+
- 					"warum die Hamburger Verantwortlichen nicht darüber in Kenntnis gesetzt "+
- 					"wurden, dazu konnte er keine Auskunft geben. Beim Unglück im vergangenen " +
- 					"Monat kamen 41 Menschen ums Leben, mehr als 200 wurden verletzt.";
-
+zDokumentText[3]="Wie ein Pressesprecher von Mauermetall gestern erklärte, ist es in einer "+
+					"firmeneigenen Erzmine zu einem Unglück gekommen bei dem 41 Menschen starben "+
+					"und mehr als 200 verletzt wurden. Grund dafür waren Sicherheitslücken, "+
+					"für die Mauermetall verantwortlich war. Wie es dazu kommen konnte und warum "+
+					"die Hamburger Verantwortlichen nicht darüber in Kenntnis gesetzt wurden, "+
+					"dazu konnte er noch keine Auskunft geben. "+
+					"Aufklärung und Fürsorge so hieß es, stünden nun vor Ort im Vordergrund.";
+zDokumentText[4]="Wie Wolfgang Maria Mayer uns gestern in einem Interview berichtete, ist es "+
+					"in einer firmeneigenen Erzmine zu einem Unglück gekommen bei dem 41 Menschen "+
+					"starben und mehr als 200 verletzt wurden. Grund dafür waren Sicherheitslücken, "+
+					"für die Mauermetall verantwortlich war. Wie es dazu kommen konnte und "+
+					"warum die Hamburger Verantwortlichen nicht darüber in Kenntnis gesetzt "+
+					"wurden, dazu konnte er keine noch Auskunft geben. "+
+					"Aufklärung und Fürsorge so erklärte er, stünden nun vor Ort im Vordergrund. ";
 zDokumentText[7]="Sicherheitslücken bei Mayermetall! Im Interview gab Wolfgang "+
  					"Maria Mayer nun zu, dass es signifikante Sicherheitsmängel "+
  					"in der verunglückten Mine gabt. Wie es dazu kommen konnte und "+
  					"warum die Hamburger Verantwortlichen nicht darüber in Kenntnis gesetzt "+
- 					"wurden, dazu konnte er keine Auskunft geben. Beim Unglück im vergangenen " +
+ 					"wurden, dazu konnte er keine Auskunft geben. Beim Unglück im vergangenen "+
  					"Monat kamen 41 Menschen ums Leben, mehr als 200 wurden verletzt.";
 zDokumentText[9]="Mauermetall-Unglück weiterhin ungeklärt! Im Interview erklärte Wolfgang "+
 					"Maria Mayer nun, dass es keinerlei Sicherheitsmängel in der verunglückten "+
@@ -46,55 +47,7 @@ zDokumentText[14]="Mayermetall-Skandal! Nun ist klar: Die Sicherheitslücken war
 					"mehr als 200 wurden verletzte und Wolfgang Maria Mayer übernimmt nach "+
 					"wie vor keinerlei Verantwortung.";
 zDokumentText[19]="„Die Mayermetall GmbH freut sich außerordentlich verkünden zu dürfen, dass "+ 
-					"der langjährige Kollege Herr Heinz Bartelsmann zum 1. September die Stelle des Personalchefs überneh
-					+
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					men wird.“ "+
+					"der langjährige Kollege Herr Heinz Bartelsmann zum 1. September die Stelle des Personalchefs übernehmen wird.“ "+
 					"Mit diesen Worten verrät Pressesprecher Knoll, was Viele erwartet haben. Durch seine "+
 					"konsequente und sichere Leitung führte er seine Abteilung stets zu Wachstum und überzeugte mit langem Atem. "+
 					"Dem pensionierten Herrn Mutmacher wünscht die IndustrieMarkt-Redaktion einen lebhaften und ausgeglichenen Ruhestand! ";
@@ -109,8 +62,8 @@ zDokumentText[20]="„Die Mayermetall GmbH freut sich außerordentlich verkünde
 
 zQuelle[0]="";					
 					
-zQuelle[3]="Zeitung 6";
-zQuelle[4]="Zeitung 7";
+zQuelle[3]="Zeitung des Nordens";
+zQuelle[4]="Zeitung des Nordens";
 zQuelle[7]="Stader Morgenblatt";
 zQuelle[9]="Stader Morgenblatt";
 zQuelle[12]="Hamburger Abendpost";
